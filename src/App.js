17e774295d977d5fc7335824c3198488687880e1
Mohammad <mohammadauf11@gmail.com>
2021-10-24 12:51:39 -0700

    adding componet and pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from "react";
+import Home from "./Pages/Home";
+import Starred from "./Pages/Starred";
+import Error from "./Pages/Error";
+import Nav from "./Component/Nav";
+import { Switch, Route } from "react-router";
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello from react</h1>
-    </div>
-  )
-}
+    <>
+      <Nav />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/starred">
+          <Starred />
+        </Route>
+        <Route>
+          <Error />
+        </Route>
+      </Switch>
+    </>
+  );
+};
 
-export default App
+export default App;
---
+++ src/Pages/Starred.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Starred = () => {
+  return <> This is starred app</>;
+};
+
+export default Starred;
